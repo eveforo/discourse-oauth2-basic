72446ba8ab49d9bc6c43e420257742c38147c1d7
iviguera <ivanviguera@gmail.com>
2019-10-08 01:43:02 +0200

    eve login button added.

--- plugin.rb
+++ plugin.rb
@@ -184,7 +184,7 @@ class OAuth2BasicAuthenticator < Auth::ManagedAuthenticator
   end
 end
 
-auth_provider title_setting: "oauth2_button_title",
+auth_provider title_setting: "",
               authenticator: OAuth2BasicAuthenticator.new,
               message: "OAuth2",
               full_screen_login_setting: "oauth2_full_screen_login"
@@ -192,11 +192,17 @@ auth_provider title_setting: "oauth2_button_title",
 register_css <<CSS
 
   button.btn-social.oauth2_basic {
-    background-color: #6d6d6d;
     background-image: url("https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-white-small.png");
     background-repeat: no-repeat;
+    background-size: cover;
+    border-radius: 5px;
   }
-
+  button.btn-social.oauth2_basic * {
+    display: none;
+  }
+    button.btn-social.oauth2_basic:hover {
+        background-image: url("https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-small.png");
+    }
 CSS
 
 load File.expand_path("../lib/validators/oauth2_basic/oauth2_fetch_user_details_validator.rb", __FILE__)
